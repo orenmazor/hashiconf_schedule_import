4224113587a6ca3c23abddbdd393660bffc311c7
Oren Mazor <oren.mazor@protonmail.com>
2019-09-10 12:04:05 -0400

    hard set the PST timezone

--- create_ics_files.py
+++ create_ics_files.py
@@ -1,11 +1,14 @@
 from icalendar import Calendar, Event
 import json
 import dateutil.parser
+import pytz
 
 # this calendar will have all of the events
 the_everything_calendar = Calendar()
 the_everything_calendar.add("prodid", "-//HashiConf2019//mxm.dk//")
 the_everything_calendar.add("version", "2.0")
+
+pst_timezone = pytz.timezone("US/Pacific")
 
 for event_json in json.load(open("schedule.json")):
     one_time_calendar = Calendar()
@@ -15,8 +18,16 @@ for event_json in json.load(open("schedule.json")):
     event = Event()
     event.add("summary", event_json["title"])
     event.add("location", event_json["location"])
-    event.add("dtstart", dateutil.parser.parse(event_json["start_time"]))
-    event.add("dtend", dateutil.parser.parse(event_json["end_time"]))
+
+    start_time = dateutil.parser.parse(event_json["start_time"])
+    end_time = dateutil.parser.parse(event_json["end_time"])
+
+    # make sure everything is in pacific
+    start_time = start_time.replace(tzinfo=pst_timezone)
+    end_time = end_time.replace(tzinfo=pst_timezone)
+
+    event.add("dtstart", start_time)
+    event.add("dtend", end_time)
 
     one_time_calendar.add_component(event)
     f = open(f"ics_files/{event_json['title']}.ics", "wb")
